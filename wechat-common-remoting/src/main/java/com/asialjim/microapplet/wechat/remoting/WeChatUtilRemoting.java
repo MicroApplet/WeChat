0a12524c97a99ef82038b9e9848ebfda62eede38
Asial Jim <asialjim@hotmail.com>
2025-03-16 18:05:10 +0800

    init
    Signed-off-by: Asial Jim <asialjim@hotmail.com>

--- wechat-common-remoting/src/main/java/com/asialjim/microapplet/wechat/remoting/WeChatUtilRemoting.java
+++ wechat-common-remoting/src/main/java/com/asialjim/microapplet/wechat/remoting/WeChatUtilRemoting.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-
+package com.asialjim.microapplet.wechat.remoting;
 import com.asialjim.microapplet.remote.http.annotation.HttpMapping;
 import com.asialjim.microapplet.remote.http.annotation.HttpMethod;
 import com.asialjim.microapplet.remote.http.annotation.HttpQuery;
@@ -21,6 +21,7 @@ import com.asialjim.microapplet.remote.http.annotation.body.JsonBody;
 import com.asialjim.microapplet.remote.net.annotation.Server;
 import com.asialjim.microapplet.wechat.constant.WeChatCons;
 import com.asialjim.microapplet.wechat.remoting.context.BaseWeChatApiRes;
+import com.asialjim.microapplet.wechat.remoting.context.WeChatAccessTokenParam;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
