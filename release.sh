7d6bcc81b8302c58a369d4acb0988b22bfab4f71
Asial Jim <asialjim@hotmail.com>
2025-03-18 21:34:36 +0800

    init
    Signed-off-by: Asial Jim <asialjim@hotmail.com>

--- release.sh
+++ release.sh
@@ -20,6 +20,8 @@ RELEASE_VERSION=${CURRENT_VERSION%-SNAPSHOT}
 NEW_VERSION=$(echo $RELEASE_VERSION | awk -F. '{$NF = $NF + 1; print $0}' | sed 's/ /./g')-SNAPSHOT
 
 # 执行自动化发布
-mvn clean release:prepare -B -DreleaseVersion=$RELEASE_VERSION  -DdevelopmentVersion=$NEW_VERSION  -Dtag=v$RELEASE_VERSION
+#mvn clean release:prepare -B -DreleaseVersion=$RELEASE_VERSION  -DdevelopmentVersion=$NEW_VERSION  -Dtag=v$RELEASE_VERSION
+#mvn release:perform -B
 
+mvn clean release:prepare -B -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$NEW_VERSION
 mvn release:perform -B
